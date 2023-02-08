92cab6080365fd31e6807e1e9927ba5792211ffe
ajitspy <124775407+ajitspy@users.noreply.github.com>
2023-02-09 01:15:08 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,26 @@
+# star pattern 1
+for i in range(4):
+    for i in range(5):
+        print("*",end=" ")
+    print()
+#star pattern 2
+for i in range(4):
+    print("*", end="")
+print()
+for i in range(2):
+    print("*",end="  ")
+print()
+for i in range(2):
+    print("*",end="  ")
+print()
+for i in range(4):
+    print("*", end="")
+#pattern 3
+n=7
+for i in range(7):
+    for j in range(i+1):
+        print("*", end="")
+    print()
+
+   
+   
